0de72db502bc5608303d95223430be108e22948f
DevHyperCoder <devan@devhypercoder.com>
2024-05-15 09:38:16 +0530

    Styling and redo homepage

---
+++ src/lib/components/SimulationCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let id: string;
+	export let name: string;
+</script>
+
+<div class="sim-card">
+	<div class="flex items-center justify-between mb-1">
+		<h3>{name}</h3>
+		<a href="/sim/{id}">View</a>
+	</div>
+	<slot />
+</div>
+
+<style>
+	.sim-card {
+		@apply bg-white p-3;
+	}
+
+	a {
+		@apply text-blue-800 underline;
+	}
+
+	h3 {
+		@apply font-semibold text-lg;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
-	import { sims } from '$lib/sims';
+	import SimulationCard from '$components/SimulationCard.svelte';
 </script>
 
 <main>
 	<h1 class="">SciSim</h1>
 	<p>This is a collection of a few simulations I made</p>
 
-	{#each sims as sim}
-		<div>
-			<h3>{sim.name}</h3>
-			<p>{sim.description}</p>
-			<a href="/sim/{sim.id}">See simulation</a>
-		</div>
-	{/each}
+	<div class="grid grid-cols-1 gap-4">
+		<SimulationCard name="Atwood Machine" id="atwood-machine">
+			<p>
+				Simulation of a mass-pulley system where you can adjust the mass (and velocity) of the
+				objects and gravity and see how the system responds.
+			</p>
+		</SimulationCard>
+
+		<SimulationCard name="Inclined Plane" id="inclined-plane">
+			<p>
+				Simulation of a inclined plane system where you can adjust the mass, angle of the plane and
+				apply dynamic friction.
+			</p>
+		</SimulationCard>
+	</div>
 </main>
 
 <style>
