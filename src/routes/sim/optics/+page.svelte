b9f9b9f29257cc25cc0eabcf9c81eba0db55ec1b
DevHyperCoder <devan@devhypercoder.com>
2024-05-27 09:33:54 +0530

    Seperate out reflection visualiser

--- src/routes/sim/optics/+page.svelte
+++ src/routes/sim/optics/+page.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
-	import NumField from '$components/NumField.svelte';
 	import ReflectionVisualizer from './ReflectionVisualizer.svelte';
-
-	let surfaceAngleDegrees: number = 0;
-	$: surfaceAngleRad = (surfaceAngleDegrees * Math.PI) / 180;
-
-	let ia_apply: 'surface' | 'normal' = 'normal';
-
-	let incidentAngleDegrees: number = 0;
-
-	$: incidentWithNormalDegrees =
-		ia_apply === 'normal' ? incidentAngleDegrees : 90 - incidentAngleDegrees;
-	$: incidentWithSurfaceRad = ((90 - incidentWithNormalDegrees) * Math.PI) / 180;
 </script>
 
 <main>
@@ -20,61 +8,11 @@
 		<p>Simulations showing some concepts of optics</p>
 	</header>
 
-	<section>
-		<h2>Reflection</h2>
-
-		<hr />
-		<h2>Inputs</h2>
-
-		<div class="input-cluster">
-			<NumField
-				id="surface-angle"
-				placeholder="angle of the surface"
-				bind:value={surfaceAngleDegrees}>Surface angle</NumField
-			>
-			<NumField
-				id="incident-angle"
-				placeholder="angle of the incident ray to the normal"
-				min={-90}
-				max={90}
-				bind:value={incidentAngleDegrees}>Incident angle</NumField
-			>
-
-			<label for="u_apply">For</label>
-			<div id="ia_apply" role="radiogroup">
-				<label for="ia_surface">Surface</label>
-				<input id="ia_surface" type="radio" bind:group={ia_apply} value="surface" />
-				<label class="ml-3" for="ia_normal">Normal</label>
-				<input id="ia_normal" type="radio" bind:group={ia_apply} value="normal" />
-			</div>
-		</div>
-
-		<hr />
-
-		<h2>Visualisation</h2>
-		<div class="grid grid-cols-2">
-			<ReflectionVisualizer {surfaceAngleRad} {incidentWithSurfaceRad} />
-			<div>
-				<p>Legend:</p>
-				<ul>
-					<li><span class="bg-[red]"></span> Reflecting Surface</li>
-					<li><span class="bg-[green]"></span> Normal</li>
-					<li><span class="bg-[yellow]"></span> Incident Ray</li>
-					<li><span class="bg-[blue]"></span> Reflecting Ray</li>
-				</ul>
-			</div>
-		</div>
-	</section>
+	<ReflectionVisualizer />
 </main>
 
 <style>
-	li {
-		@apply flex gap-2 mt-1 mb-1;
-	}
-	span {
-		@apply h-4 w-4 block;
-	}
-	.input-cluster {
+	:global(.input-cluster) {
 		@apply grid-cols-[16ch_1fr];
 	}
 </style>
--- src/routes/sim/optics/ReflectionVisualizer.svelte
+++ src/routes/sim/optics/ReflectionVisualizer.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { Vector } from '$lib/Vector';
+	import NumField from '$components/NumField.svelte';
 
-	export let surfaceAngleRad: number;
-	export let incidentWithSurfaceRad: number;
+	let surfaceAngleDegrees: number = 0;
+	$: surfaceAngleRad = (surfaceAngleDegrees * Math.PI) / 180;
+
+	let ia_apply: 'surface' | 'normal' = 'normal';
+
+	let incidentAngleDegrees: number = 0;
+
+	$: incidentWithNormalDegrees =
+		ia_apply === 'normal' ? incidentAngleDegrees : 90 - incidentAngleDegrees;
+	$: incidentWithSurfaceRad = ((90 - incidentWithNormalDegrees) * Math.PI) / 180;
 
 	let canvas: HTMLCanvasElement;
 	let context: CanvasRenderingContext2D;
@@ -131,4 +140,55 @@
 	}
 </script>
 
-<canvas bind:this={canvas} width="400" height="400" />
+<section>
+	<h2>Reflection</h2>
+
+	<hr />
+	<h2>Inputs</h2>
+
+	<div class="input-cluster">
+		<NumField id="surface-angle" placeholder="angle of the surface" bind:value={surfaceAngleDegrees}
+			>Surface angle</NumField
+		>
+		<NumField
+			id="incident-angle"
+			min={-90}
+			max={90}
+			placeholder="angle of the incident ray to the normal"
+			bind:value={incidentAngleDegrees}>Incident angle</NumField
+		>
+
+		<label for="u_apply">For</label>
+		<div id="ia_apply" role="radiogroup">
+			<label for="ia_surface">Surface</label>
+			<input id="ia_surface" type="radio" bind:group={ia_apply} value="surface" />
+			<label class="ml-3" for="ia_normal">Normal</label>
+			<input id="ia_normal" type="radio" bind:group={ia_apply} value="normal" />
+		</div>
+	</div>
+
+	<hr />
+
+	<h2>Visualisation</h2>
+	<div class="grid grid-cols-2">
+		<canvas bind:this={canvas} width="400" height="400" />
+		<div>
+			<p>Legend:</p>
+			<ul>
+				<li><span class="bg-[red]"></span> Reflecting Surface</li>
+				<li><span class="bg-[green]"></span> Normal</li>
+				<li><span class="bg-[yellow]"></span> Incident Ray</li>
+				<li><span class="bg-[blue]"></span> Reflecting Ray</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<style>
+	li {
+		@apply flex gap-2 mt-1 mb-1;
+	}
+	span {
+		@apply h-4 w-4 block;
+	}
+</style>
