e140bbe8c8e7d947abbe629859206b6adf0f499a
DevHyperCoder <devan@devhypercoder.com>
2024-05-31 12:04:05 +0530

    wrapper around a canvas to make it full width

---
+++ src/lib/components/FullWidthCanvas.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { DrawFn } from '$lib/types';
+	import Canvas from './Canvas.svelte';
+
+	let clientWidth: number = 100;
+
+	$: width = clientWidth;
+	$: height = clientWidth;
+
+	export let draw: DrawFn;
+
+	let _canvas: HTMLCanvasElement;
+	export let canvas: HTMLCanvasElement | null = null;
+
+	$: canvas = _canvas;
+</script>
+
+<div bind:clientWidth class="w-100">
+	<Canvas bind:canvas={_canvas} {width} {height} {draw} />
+</div>
