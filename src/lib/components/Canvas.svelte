8bd548511cc9a65cf71a7e94b57c7b6e148f31fb
DevHyperCoder <devan@devhypercoder.com>
2024-05-31 12:03:41 +0530

    Make exported canvas instance prop optional

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -6,18 +6,22 @@
 	export let height: number;
 	export let draw: DrawFn;
 
-	export let canvas: HTMLCanvasElement;
+	let _canvas: HTMLCanvasElement;
+	export let canvas: HTMLCanvasElement | null = null;
+
+	$: canvas = _canvas;
+
 	let context: CanvasRenderingContext2D;
 	let ready = false;
 
 	onMount(() => {
-		context = canvas.getContext('2d')!;
+		context = _canvas.getContext('2d')!;
 		ready = true;
 	});
 
 	$: if (ready) draw(context, width, height);
 </script>
 
-<canvas bind:this={canvas} {width} {height}
+<canvas bind:this={_canvas} {width} {height}
 	><a href="https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API">Canvas API</a> is required.</canvas
 >
